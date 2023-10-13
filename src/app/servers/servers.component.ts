7c97da62a4dc0291ab3279076c82e5506a7ca8fa
“Salvo1789” <salvo.mercurio89@gmail.com>
2023-10-13 19:21:07 +0200

    Data binding exercises

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer: boolean = false;
+  hasUsername: boolean = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  username = "";
 
+  constructor() {
+    setTimeout( () => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+  
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Its name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUsername(event: Event){
+    this.hasUsername = true;
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  resetUsername(){
+    this.username="";
+    this.hasUsername=false;
+  }
 }
